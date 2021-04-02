e0c5e98de561931bf6218980e40f4f6c304ad90c
Matt Thompson <matt@matteveryday.com>
2021-04-02 07:23:18 -0400

    test deploy

--- components/layout.js
+++ components/layout.js
@@ -1,14 +1,15 @@
-import Alert from '../components/alert'
-import Footer from '../components/footer'
+import Alert from "../components/alert";
+import Footer from "../components/footer";
 
 export default function Layout({ preview, children }) {
   return (
     <>
       <div className="min-h-screen">
         <Alert preview={preview} />
+        <p>Im a paragraph</p>
         <main>{children}</main>
       </div>
       <Footer />
     </>
-  )
+  );
 }
